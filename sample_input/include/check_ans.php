2398674960528ee59b29233dd091ca9269174ab6
skieer-ssv <55557885+skieer-ssv@users.noreply.github.com>
2021-09-01 03:30:52 +0530

    ans checking logic added

--- sample_input/include/check_ans.php
+++ sample_input/include/check_ans.php
@@ -1,11 +1,20 @@
 <?php
+session_start();
 if(isset($_POST['submit'])){
-  $userAns=password_hash(strtolower(strip_tags($_POST['answer'])),PASSWORD_DEFAULT);
-    if($userAns==$_SESSION['currentAns']){
-$_SESSION['newQuesFlag']=1; //new ques will be fetched
+  $userAns=strtolower(strip_tags($_POST['answer']));
+  
+  
+    if(password_verify($userAns, $_SESSION['currentAns'])){
+      $_SESSION['newQuesFlag']=1;
+      echo '<script>alert("correct");document.location="../question.php";</script>';
+ //new ques will be fetched
+    }
+    else{
+      header('location: ../question.php?msg="wrong ans"');
     }
 }
 else{
-    header('location: ../question.php?msg="wrong"');
+  
+    header('location: ../question.php?msg="not submitted"');
 }
 ?>
