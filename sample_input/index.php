47b0286cee3420dbc7ebf78a21cbc93a735062e0
hkaranjule77 <hkaranjule77@gmail.com>
2021-09-01 01:18:11 +0530

    include/check_final_ans added

--- sample_input/index.php
+++ sample_input/index.php
@@ -1,14 +1,17 @@
 <?php
     
-    require './dbconfig.php';
-    if( isset($POST['submit']) ){
+    require './include/dbconfig.php';
 
+    if( isset($POST['submit']) ){
+        
+        # data-retrieval
         $lead_name = mysqli_real_escape_string($connection, $POST['']);
         $mob_no = mysqli_real_escape_string($connection, $POST['']);
         $branch = mysqli_real_escape_string($connection, $POST['']);
         $year = mysqli_real_escape_string($connection, $POST['']);
         $curr_time = time();
 
+        # query-processing
         $login_query = "INSERT INTO `users` (`lead_name`, `mob_no`, `branch`, `year`, `reg_time`) VALUES('$lead_name', '$mob_no', '$branch', '$year', '$curr_time')";
         $result = mysqli_query($connection, $login_query);
         
