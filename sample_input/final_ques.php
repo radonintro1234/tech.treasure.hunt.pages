a5a7ee734fbdd5c9b2905238a8d44f59fba054c2
skieer-ssv <55557885+skieer-ssv@users.noreply.github.com>
2021-09-01 16:35:58 +0530

    last question pages added

---
+++ sample_input/final_ques.php
@@ -0,0 +1,15 @@
+<?php 
+session_start();
+if($_SESSION['user'] && $_SESSION['final']){
+
+
+
+
+    
+
+}
+else{
+    header('location:index.html');
+}
+
+
--- sample_input/include/check_final_ans.php
+++ sample_input/include/check_final_ans.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
     include './dbconfig.php';
 
     if(isset($POST['submit'])){
@@ -6,12 +7,14 @@
         $answer = mysqli_real_escape_string($connection, $_POST['']);
         $answer = strtolower($answer);                                  # converts to lowercase
         if( ! strcmp($answer, 'stealthy')){                             # compares with stealthy
-            return true;
+           $_SESSION['heist_solved']=1;
+            header('location:../heist_completed.php');
         }
         else{
             echo "
             <script>
                 alert('Wrong answer, please try again!');
+                document.location='../final_question.php';
             </script>";
             return false;
         }
