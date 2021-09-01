924cfc7f9974213bd5e70b28a1ee02c652e61d57
hkaranjule77 <hkaranjule77@gmail.com>
2021-09-01 20:17:45 +0530

    reg submit registration

--- sample_input/include/login.php
+++ sample_input/include/login.php
@@ -8,13 +8,11 @@
         # data-retrieval
         $team_name = mysqli_real_escape_string($con, $_POST['team-name']);
         $passcode = mysqli_real_escape_string($con, $_POST['passcode']);
-        $curr_time = time();
 
         # fetch-credentials
         $login_query = "SELECT * FROM `credentials` WHERE `team_name`='$team_name' AND `passcode`='$passcode';";
         echo $login_query;
         $result = mysqli_query($con, $login_query);
-        //echo "<br> row " + mysqli_num_rows($result);
         
         if( mysqli_num_rows($result) == 0 ){
             echo "
@@ -23,7 +21,11 @@
             </script>";
         }
         else{
-            echo "reached 4";
+            # records registration time
+            $curr_time = time();
+            $reg_time_query = "INSERT INTO `records`(`team_name`, `reg_time`) VALUES('$team_name', NOW());";
+            $result = mysqli_query($con, $reg_time_query);
+
             $_SESSION['user'] = $team_name;
             require_once './initial_config.inc.php';
             header('location: ../question.php');
