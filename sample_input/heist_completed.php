bef3df96f790f06634810491e3b0b1c71fa4c7f4
yash-png <65919365+yash-png@users.noreply.github.com>
2021-09-02 19:33:52 +0530

    final page and secret link added

--- sample_input/heist_completed.php
+++ sample_input/heist_completed.php
@@ -1,6 +1,6 @@
 <?php 
 session_start();
-if(isset($_SESSION['user'])&& $_SESSION['heist_solved']){
+if(true || isset($_SESSION['user'])&& $_SESSION['heist_solved'] ){
     
 ?>
 <!doctype html>
@@ -15,22 +15,27 @@ if(isset($_SESSION['user'])&& $_SESSION['heist_solved']){
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@200&family=ZCOOL+KuaiLe&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
-    <link href="https://fonts.googleapis.com/css2?family=Cinzel&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Cinzel+Decorative&display=swap" rel="stylesheet">
     <title>Home</title>
+    <style>
+    body {
+            overflow-y: hidden ;
+            overflow-x: hidden;
+        }
+    </style>
 </head>
 
-<body class="winnerDiv">
-
-
-    <div class="container  text-light mt-5">
-        <h2>TIME TAKEN :
+<body class="winnerDiv border border-light ">
+    <div class="container text-light " style="margin-top:20rem;">
+        <h2 class="text-center"style="font-size:2.5rem;">TIME TAKEN :
             <?php
 
         echo $_SESSION['time_taken_by_team'];
 
        ?>
-        </h2>
-    </div>
+        </h2> 
+    </div> 
+        <h1 id="hiddentext" class="text-light text-center mt-4">This Is Not The True Ending..<snap><a class="secretlink" href="#">.</a></snap>..</h1> 
 </body>
 
 </html>
